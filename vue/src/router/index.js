771ad78fc9e3a9015947c9b2fb74c59f5bd7f5f5
alamgirweb11 <alamgirweb11@gmail.com>
2022-05-19 23:49:31 +0600

    add some layouts static pages

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,32 @@
 import { createRouter, createWebHistory } from "vue-router";
+import DefaultLayout from '../components/DefaultLayout.vue'
+import Dashboard from '../views/Dashboard.vue'
+import Login from '../views/auth/Login.vue'
+import Register from '../views/auth/Register.vue'
+import Surveys from '../views/pages/Surveys.vue'
 
-const routes = []
+const routes = [
+        {
+               path: '/',
+               redirect: '/dashboard',
+               name: 'Dashboard',
+               component: DefaultLayout,
+               children:[
+                       {path: '/dashboard', name: 'Dashboard', component: Dashboard},
+                       {path: '/surveys', name: 'Surveys', component: Surveys}
+               ]
+        },
+        {
+               path: '/login',
+               name: 'Login',
+               component: Login,
+        },
+        {
+               path: '/register',
+               name: 'Register',
+               component: Register
+        },
+]
 
 const router = createRouter({
        history: createWebHistory(),
