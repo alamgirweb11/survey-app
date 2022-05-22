caa213adb9d4f62de98928fb31082b736e683b41
alamgirweb11 <alamgirweb11@gmail.com>
2022-05-22 23:45:34 +0600

    login from frontend

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,5 +1,6 @@
 import {createStore} from 'vuex'
 import axiosClient from '../axios'
+
 const store = createStore({
        state: {
           user: {
@@ -9,18 +10,6 @@ const store = createStore({
        },
        getters: {},
 
-       mutations: {
-        logout: (state) => {
-                 state.user.data = {},
-                 state.user.token = null
-        },
-         setUser: (state, userData) => {
-                state.user.token = userData.token  
-                state.user.data = userData.user
-                sessionStorage.setItem('TOKEN', userData.token)  
-         }
-       },
-
        actions: {
                register({commit}, user){
                    return axiosClient.post('/register', user)
@@ -28,7 +17,28 @@ const store = createStore({
                             commit('setUser', data)
                             return data;
                    }) 
+               },
+
+               login({commit}, user){
+                   return axiosClient.post('/login', user)
+                   .then(({data}) => {
+                            commit('setUser', data)
+                            return data;
+                   }) 
                }
+       },
+
+       mutations: {
+        logout: (state) => {
+                 state.user.data = {},
+                 state.user.token = null,
+                 sessionStorage.removeItem('TOKEN')
+        },
+         setUser: (state, userData) => {
+                state.user.token = userData.token  
+                state.user.data = userData.user
+                sessionStorage.setItem('TOKEN', userData.token)  
+         }
        },
        
        modules: {},
