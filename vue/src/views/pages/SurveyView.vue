a2e70f390824043fac1fff864733261c19b17dd9
alamgirweb11 <alamgirweb11@gmail.com>
2022-06-11 01:26:34 +0600

    survey form added

--- vue/src/views/pages/SurveyView.vue
+++ vue/src/views/pages/SurveyView.vue
@@ -1,14 +1,163 @@
 <template lang="">
-<PageContent>
- Survey view page create..
-</PageContent>
+  <PageContent>
+    <template v-slot:header>
+      <div class="flex items-center justify-between">
+        <h1 class="text-3xl font-bold text-gray-900">
+          {{ model.id ? model.title : "Create new survey" }}
+        </h1>
+      </div>
+    </template>
+    <form @submit.prevent="saveSurvey">
+      <div class="shadow sm:rounded-md sm:overflow-hidden">
+        <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+          <!-- image -->
+          <div>
+            <label class="block text-sm font-medium text-gray-700">Image</label>
+            <div class="mt-1 flex items-center">
+              <img
+                v-if="model.image"
+                :src="model.image"
+                :alt="model.title"
+                class="w-64 h-48 object-cover"
+              />
+              <label
+                v-else
+                class="flex items-center justify-center h-12 w-12 rounded-full overflow-hidden bg-gray-100"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-[80%] w-[80%] text-gray-800"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                  />
+                </svg>
+              </label>
+
+              <button
+                type="button"
+                class="relative overflow-hidden ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <input
+                  type="file"
+                  class="absolute left-0 top-0 right-0 bottom-0 opacity-0 cursor-pointer"
+                />
+                Change
+              </button>
+            </div>
+            <!-- / image -->
+            <!-- title -->
+            <div>
+              <label for="title" class="block text-sm font-medium text-gray-700"
+                >Title</label
+              >
+              <input
+                type="text"
+                name="title"
+                id="title"
+                v-model="model.title"
+                autocomplete="survey_title"
+                class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              />
+            </div>
+            <!-- / title -->
+            <!-- description -->
+            <div class="mt-3">
+              <label
+                for="description"
+                class="block text-sm font-medium text-gray-700"
+                >Description</label
+              >
+              <div class="mt-1">
+                <textarea
+                  type="textarea"
+                  name="description"
+                  id="description"
+                  v-model="model.description"
+                  autocomplete="survey_description"
+                  class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <!-- / description -->
+            <!-- expire date -->
+            <div class="mt-3">
+              <label
+                for="expire_date"
+                class="block text-sm font-medium text-gray-700"
+                >Description</label
+              >
+              <input
+                type="date"
+                name="expire_date"
+                id="expire_date"
+                v-model="model.expire_date"
+                autocomplete="survey_expire_date"
+                class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              />
+            </div>
+            <!-- / expire date -->
+            <!-- status -->
+            <div class="mt-3 flex items-start">
+              <div class="flex items-center h-5">
+                <input
+                  type="checkbox"
+                  name="status"
+                  id="status"
+                  v-model="model.status"
+                  class="mt-1 h-5 w-5 focus:ring-indigo-500 text-indigo-600 border-gray-300 rounded"
+                />
+              </div>
+
+              <div class="ml-3 text-sm"></div>
+              <label for="status" class="font-medium text-gray-700"
+                >Active</label
+              >
+            </div>
+            <!-- / status -->
+            <!-- submit button -->
+            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+              <button type="submit"
+               class="
+               inline-flex justify-center py-2 px-4 border border-transparent
+               shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none
+                focus:right-2 focus:ring-offset-2 focus:ring-indigo-500
+               ">Save</button>
+            </div>
+            <!-- / submit button -->
+          </div>
+        </div>
+      </div>
+    </form>
+  </PageContent>
 </template>
-<script>
-import PageContent from '../../components/PageContent.vue'
-export default {
-    
+<script setup>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+import store from "../../store";
+
+import PageContent from "../../components/PageContent.vue";
+
+const route = useRoute();
+
+// create empty survey
+let model = ref({
+  title: "",
+  status: false,
+  description: null,
+  image: null,
+  expire_date: null,
+  expire_date: null,
+  questions: [],
+});
+
+if (route.params.id) {
+  model.value = store.state.surveys.find(
+    (s) => s.id === parseInt(route.params.id)
+  );
 }
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
