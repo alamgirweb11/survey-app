978fed455a7dfe9f90e5c88425f4dd110d769192
alamgirweb11 <alamgirweb11@gmail.com>
2022-06-20 23:54:20 +0600

    make question viewer component

--- vue/src/components/viewer/QuestionViewer.vue
+++ vue/src/components/viewer/QuestionViewer.vue
@@ -1,7 +1,49 @@
 <template lang="">
- View Questions
+   <fieldset class="mb-4">
+     <div>
+        <legend class="text-base font-medium text-gray-900">
+            {{ index + 1 }}. {{ question.question }}
+        </legend>
+        <p class="text-gray-500 text-sm">
+            {{ question.description }}
+        </p>
+     </div>
+
+     <div class="mt-3">
+        <div v-if="question.type === 'select'">
+        
+        </div>
+        <div v-else-if="question.type === 'radio'">
+
+        </div>
+        <div v-else-if="question.type === 'checkbox'">
+
+        </div>
+        <div v-else-if="question.type === 'text'">
+
+        </div>
+        <div v-else-if="question.type === 'textarea'">
+        
+        </div>
+     </div>
+   </fieldset>
+   <hr class="mb-4"/>
 </template>
 <script setup>
+  import { ref } from 'vue';
+   const { question, index, modelValue } = defineProps({
+       question: Object,
+       index: Number,
+       modelValue: [String, Array],
+   });
+
+  const emits = defineEmits(['update:modelValue']);
+
+  let model;
+
+  if(question.type === 'checkbox'){
+    model = ref({});
+  }
 
 </script>
 <style scoped>
